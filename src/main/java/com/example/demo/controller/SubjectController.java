91c57b14b36aa9fb515d20b7414d2ced2a64ef02
sangaychophel <sangchop97@gmail.com>
2023-06-17 10:26:10 +0600

    First commit

---
+++ src/main/java/com/example/demo/controller/SubjectController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.bean.Subject;
+import com.example.demo.services.SubjectService;
+
+
+@RestController
+public class SubjectController {
+	
+	@Autowired
+	private SubjectService subjectService;
+	
+	@RequestMapping ("/subjects")
+	public List<Subject> getAllSubjects()
+	{
+		return subjectService.getAllSubjects();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/subjects")
+	public void addsubject(@RequestBody Subject subject) 
+	{
+		subjectService.addSubject(subject);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/subjects/{id}")
+	public void updatesubject(@PathVariable String id, @RequestBody Subject subject) 
+	{
+		subjectService.updateSubject(id, subject);
+	}
+	@RequestMapping(method = RequestMethod.DELETE, value = "/subjects/{id}")
+	public void deleteSubject(@PathVariable String id) {
+		subjectService.deleteSubject(id);
+	}
+
+}
